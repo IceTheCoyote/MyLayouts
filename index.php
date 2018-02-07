4fbf13de63580ec2097058707553edac177a2e8f
TWSICommunity <gamewolf10@gmail.com>
2018-02-07 11:27:04 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php session_start(); ?>
+<html>
+ <head>
+  <title>Example Sheet</title>
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+ </head>
+ <body onload="loading()">
+  <div class = "standardPage">
+   <div class = "frame">
+    <div class = "frameLogo">
+     <a href='#' onclick='loading()'><img src = "images/ui/logo.png" /></a>
+    </div>
+    <div class = "statusLinks">
+     <div class = "statusLogin" id = "statusLogin_">
+	    <b>Your login and register link here.</b>
+	 </div>
+	</div>
+	<div class = "statusNews">
+	 <b>Status News Example</b>
+	</div>
+   </div>
+   <div class = "clientPage" id = "client">
+    <b>Styling my hair...</b>
+   </div>
+  </div>
+  <script src="your javascript here"></script>
+ </body>
+</html>
